5fa84df67e42ade862bbc8bcf672b1197e1d5cd3
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-07-31 22:26:24 +0900

    노마드 코더 Flutter 프로젝트 생성 및 5.0 실습

---
+++ pomodoro/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: Color(0xFFE7626C),
+        ),
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: Color(0xFFF4EDDB),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
