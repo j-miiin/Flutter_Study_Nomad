16cad813686a8fdfbd11938e06366e7e6b9f254b
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-08-03 18:32:11 +0900

    노마드 코더 Flutter 6.5 ~ 6.6 실습

--- toonflix/lib/screen/home_screen.dart
+++ toonflix/lib/screen/home_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +25,32 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+      body: FutureBuilder(
+        future: webtoons, // FutureBuilder를 통해 webtoons를 기다려줄 수 있음
+        builder: (context, snapshot) {
+          // snapshot으로 Future의 상태를 알 수 있음 (데이터를 받았는지, 오류가 났는지)
+          if (snapshot.hasData) {
+            return Text("There is Data");
+          }
+          return Text("Loading...");
+        },
+      ),
     );
   }
 }
+
+  // Stateful Widget일 때의 방법 -> 이렇게 할 필요가 없다!
+  // List<WebtoonModel> webtoons = [];
+  // bool isLoading = true;
+
+  // void waitForWebToons() async {
+  //   webtoons = await ApiService.getTodaysToons();
+  //   isLoading = false;
+  //   setState(() {});
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   waitForWebToons();
+  // }
